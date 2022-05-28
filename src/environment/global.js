d48cf5559f3cba1a480930809a018de4ada9001c
kalomcode <carloscalzadacabano@gmail.com>
2022-05-28 16:41:15 +0200

    mostrar global

---
+++ src/environment/global.js
@@ -0,0 +1,7 @@
+const url_git ='https://plabo95.github.io/klndr_front/api/';
+const url_dev ='http://127.0.0.1:8000/api/';
+const url_pro ='https://plabo.pythonanywhere.com/api/';
+
+// export const base_url = url_dev;
+export const base_url = url_pro;
+
